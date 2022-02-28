8d44e61d0b3aee50873aeee74937e4c3925da006
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-28 21:39:41 +0530

    app bar added in web

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/widgets/chat_app_bar.dart';
 import 'package:whatsapp_ui/widgets/contacts_list.dart';
 import 'package:whatsapp_ui/widgets/web_profile_bar.dart';
 import 'package:whatsapp_ui/widgets/web_search_bar.dart';
@@ -30,6 +31,13 @@ class WebScreenLayout extends StatelessWidget {
                 image: AssetImage('assets/backgroundImage.png'),
                 fit: BoxFit.cover,
               ),
+            ),
+            child: Column(
+              children: [
+                WebChatAppBar(),
+                // ChatList
+                // MessageI/PBox
+              ],
             ),
           ),
         ],
--- lib/widgets/chat_app_bar.dart
+++ lib/widgets/chat_app_bar.dart
@@ -1,54 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/colors.dart';
 
-class WebProfileBar extends StatelessWidget {
-  const WebProfileBar({Key? key}) : super(key: key);
+import '../info.dart';
+
+class WebChatAppBar extends StatelessWidget {
+  const WebChatAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.082,
-      width: MediaQuery.of(context).size.width * 0.3,
+      width: MediaQuery.of(context).size.width * 0.7,
       padding: const EdgeInsets.all(10),
-      decoration: const BoxDecoration(
-        border: Border(
-          right: BorderSide(
-            color: dividerColor,
-          ),
-        ),
-        color: webAppBarColor,
-      ),
+      color: webAppBarColor,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const CircleAvatar(
-            backgroundImage: NetworkImage(
-              'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
-            ),
-            radius: 20.0,
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  info[0]['profilePic'].toString(),
+                ),
+                radius: 20.0,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.01,
+              ),
+              Text(
+                info[0]['name'].toString(),
+                style: const TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+            ],
           ),
           Row(
             children: [
               IconButton(
                 onPressed: () {},
                 icon: const Icon(
-                  Icons.circle_outlined,
+                  Icons.search,
+                  size: 22,
                   color: Colors.grey,
                 ),
-                tooltip: 'Status',
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.comment,
-                  color: Colors.grey,
-                ),
-                tooltip: 'Messages',
+                tooltip: 'Search',
               ),
               IconButton(
                 onPressed: () {},
                 icon: const Icon(
                   Icons.more_vert,
+                  size: 22,
                   color: Colors.grey,
                 ),
                 tooltip: 'More',
